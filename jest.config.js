df97a2751cc940ccba4ccf8b624749999a2871b2
Chikako Wakishima <ChikakoW@sedata.com>
2022-03-02 16:27:47 -0500

    added esmodule lib reference and test fails

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
+const esModules = ['lodash-es', 'echarts', 'zrender'].join('|');
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   globalSetup: 'jest-preset-angular/global-setup',
+  // transformIgnorePatterns: [
+  //   'node_modules/(?!.*.mjs$)',
+  //   `/node_modules/(?!${esModules}).+(js|jsx)$`,
+  // ],
 };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { startCase } from 'lodash-es';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng13-jest-esm-import-repro';
+  title = startCase('ng13-jest-esm-import-repro');
 }
